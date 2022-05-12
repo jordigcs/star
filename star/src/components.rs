d3f7988c21f5581c77343eca7bae7672d3f1f05b
jordi <creptthrust@gmail.com>
2022-05-12 15:11:10 -0500

    Timer functionality

--- star/src/components.rs
+++ star/src/components.rs
@@ -1,41 +1,26 @@
-use std::time::Duration;
+use std::{cell::Cell, rc::Rc};
 
-use yew::prelude::*;
-use wasm_timer::{Instant, Interval};
+use web_sys::{window, HtmlAudioElement, HtmlElement, HtmlInputElement};
+use yew::{prelude::*, props};
+use gloo_timers::callback::Interval;
 use wasm_logger::*;
-use yew_hooks::use_interval;
-#[derive(Properties, PartialEq, Clone)]
-pub struct TimerData {
-    pub active:bool,
-    #[prop_or(1800)]
-    pub time_left:u64, // Seconds
-    #[prop_or_default]
-    pub callback:Callback<u16>,
-}
-
-impl TimerData {
-    pub fn format_time_left(time_left:u64) -> String {
-        let minutes = time_left / 60;
-        let seconds = time_left - (minutes * 60);
-        
-        format!("{:02}:{:02}", minutes, seconds)
-    }
-}
+use wasm_timer::Instant;
+use crate::state::{TimerData, TimerAction};
 
 #[function_component]
 pub fn Timer(data:&TimerData) -> Html {
-    let data = data.clone();
-    let last_time = use_state(|| Instant::now());
-    let time_left = use_state(|| data.time_left.to_owned());
-    Interval::new(Duration::from_secs(1));
-    let interval = Interval::new(1000, move || {
-        if (*last_time).elapsed().as_secs() >= 1 {
-            time_left.set(*time_left - (*last_time).elapsed().as_secs());
-            last_time.set(Instant::now());
+    log::warn!("Render");
+    if data.time_left.get() <= 0 {
+        if data.timer_sound.paused() {
+            data.timer_sound.play();
         }
-    });
+    }
     html! {
-        <h2 class="timer_label">{ TimerData::format_time_left(*time_left) }</h2>
+        <>
+        <h2 class={ "timer_label".to_owned() + (if data.time_left.get() < 0 { " timer_expired"} else { "" })}>{ 
+            TimerData::format_time_left(data.time_left.get()) 
+        }</h2>
+        </>
     }
 }
 
@@ -58,21 +43,73 @@ pub struct CsData {
 
 #[function_component]
 pub fn CsCycle(data:&CsData) -> Html {
+    let timer_data = TimerData {
+        time_left:use_state(|| Cell::new(2)),
+        running: false,
+        callback:Callback::noop(),
+        timer_interval_id: -1,
+        timer_sound: HtmlAudioElement::new_with_src("src").expect("Could not load timer sound.")
+    };
     let state = use_state(|| CsState::NotStarted);
-    let destroy_card = data.destroy_card_callback.clone();
+    let timer_state = use_reducer(|| timer_data);
+    
+    let start_time_value = use_state(|| 1800);
+    let start_time_input_str = use_state(|| TimerData::format_time_left(*start_time_value));
+    let start_time_input_ref = use_node_ref();
+
     let start_cycle = {
         let state = state.clone();
-        Callback::from(move |_| state.set(CsState::Started))
+        let timer_state = timer_state.clone();
+        let start_time_value = start_time_value.clone();
+        Callback::from(move |_| {
+            timer_state.dispatch(TimerAction::Start(*start_time_value));
+            state.set(CsState::Started)
+        })
     };
-    let stop_cycle = Callback::from(move |_| destroy_card.emit(0));
+
+    let destroy_card = data.destroy_card_callback.clone();
+    let stop_cycle = {
+        let state = state.clone();
+        let timer_state = timer_state.clone();
+        Callback::from(move |_| {
+            timer_state.dispatch(TimerAction::Stop);
+            state.set(CsState::NotStarted);
+        })
+    };
+
+    let start_time_changed = {
+        let timer_state = timer_state.clone();
+        let start_time_input_ref = start_time_input_ref.clone();
+        let start_time_value = start_time_value.clone();
+        let start_time_input_str = start_time_input_str.clone();
+        Callback::from(move |_| {
+            let input = start_time_input_ref.cast::<HtmlInputElement>().expect("Timer not initialized correctly.");
+            let mut formatted_input_value:String = String::new();
+            for c in input.value().chars().into_iter() {
+                if !c.is_numeric() && c != ':' {
+                    continue;
+                }
+                formatted_input_value.push(c);
+            }
+            if let Ok(start_time) = TimerData::seconds_from_str(formatted_input_value) {
+                start_time_value.set(start_time.into());
+                start_time_input_str.set(TimerData::format_time_left(start_time.into()));
+            }
+            else {
+                start_time_input_str.set(String::new());
+            }
+        })
+    };
+
     html! {
         <>
             <div class="timer">
-            <Timer active={ *state == CsState::Started }/>
             if *state == CsState::NotStarted {
-                <button class="button" onclick={start_cycle}>{ "Start Cycle" }</button>
+                <input ref={ start_time_input_ref } onchange={ start_time_changed } value={ (*start_time_input_str).clone() }class="timer_input" size="1" type="text" />
+                <button class="button" onclick={start_cycle}><span class="material-icons icon">{ "update" }</span>{ " Start Cycle" }</button>
             }
             else {
+                <Timer time_left={ timer_state.time_left.clone() } />
                 <button class="button outlined" onclick={stop_cycle}>{ "Stop Cycle" }</button>
             }
             </div>
@@ -80,9 +117,10 @@ pub fn CsCycle(data:&CsData) -> Html {
             if *state == CsState::NotStarted {
                 <p>{ "The CS cycle begins with brewing coffee. Brew some coffee and click " }<b>{ "Start Cycle" }</b>{ " to begin." }</p>
             } else {
+                <p><b>{ "Tasks" }</b></p>
                 <Checkbox text="Brew Coffee" default_value={true} />
                 <Checkbox text="Cafe Check"/>
-                <Checkbox text="Flex"/>
+                <button class="button outlined" ><span class="material-icons">{ "add" }</span>{ " Schedule a new task" }</button>
             }
         </>
     }
--- star/src/state.rs
+++ star/src/state.rs
@@ -1,9 +1,13 @@
-use std::{rc::Rc, collections::VecDeque};
+use std::{rc::Rc, collections::VecDeque, borrow::BorrowMut, cell::Cell};
 
+use web_sys::{Window, window, HtmlAudioElement};
+use gloo_timers::callback::Interval;
+use wasm_timer::Instant;
 use yew::prelude::*;
 
 use crate::*;
 
+// Star
 #[derive(Debug)]
 pub enum StarAction {
     AddCard(CardType),
@@ -68,6 +72,133 @@ impl Reducible for StarData {
                 StarData {
                     priority_card: self.priority_card.clone(),
                     cards: self.cards.clone(),
+                }.into()
+            }
+        }
+    }
+}
+
+// Timer
+pub enum TimerAction {
+    Start(i32),
+    Stop,
+    SetCallback(Callback<u16>)
+}
+
+#[derive(Properties, PartialEq, Clone)]
+pub struct TimerData {
+    pub time_left:UseStateHandle<Cell<i32>>, // Seconds,
+    #[prop_or_default]
+    pub running:bool,
+    #[prop_or_default]
+    pub callback:Callback<u16>,
+    #[prop_or_default]
+    pub timer_interval_id:i32,
+    #[prop_or(HtmlAudioElement::new().unwrap())]
+    pub timer_sound:HtmlAudioElement,
+}
+
+pub enum TimerParseError {
+    InvalidInput
+}
+
+impl TimerData {
+    pub fn format_time_left(mut time_left: i32) -> String {
+        let mut is_negative:bool = false;
+        if time_left < 0 {
+            time_left = time_left.abs();
+            is_negative = true;
+        }
+        let minutes = time_left / 60;
+        let seconds = time_left - (minutes * 60);
+        (if is_negative { "-" } else { "" }).to_owned() + &format!("{:02}:{:02}", minutes, seconds)
+    }
+
+    
+    pub fn seconds_from_str(time:String) -> Result<u16, TimerParseError>  {
+        let split_time:Vec<&str> = time.split(':').collect();
+        let mut minutes: u16 = 0;
+        let mut seconds: u16 = 0;
+        if split_time.len() > 1 { // Time has minutes
+            // Parse Minutes
+            if let Ok(val) = split_time[0].parse::<u16>() {
+                minutes = val;
+            }
+            else {
+                return Err(TimerParseError::InvalidInput);
+            }
+            // Parse seconds
+            if let Ok(val) = split_time[1].parse::<u16>() {
+                seconds = val;
+            }
+            else {
+                return Err(TimerParseError::InvalidInput);
+            }
+        }
+        else {
+            // Parse seconds
+            if let Ok(val) = split_time[0].parse::<u16>() {
+                seconds = val;
+            }
+            else {
+                return Err(TimerParseError::InvalidInput);
+            }
+        }
+        Ok((minutes * 60) + seconds)
+    }
+
+    pub fn stop(&self) {
+        if self.timer_interval_id > -1 {
+            if let Some(window) = window() {
+                window.clear_interval_with_handle(self.timer_interval_id);
+            }
+        }
+    }
+}
+
+impl Reducible for TimerData {
+    type Action = TimerAction;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        match action {
+            TimerAction::Start(st) => {
+                let mut last_time = Instant::now();
+                let time_left = self.time_left.clone();
+                (*time_left).set((st - 1).into());
+                let timer_interval = Interval::new(350, move || {
+                    if last_time.elapsed().as_secs() >= 1 {
+                        (*time_left).set((*time_left).get() - last_time.elapsed().as_secs() as i32);
+                        time_left.set((*time_left).clone());
+                        last_time = Instant::now();
+                    }
+                });
+                let id = timer_interval.forget();
+
+                Self {
+                    time_left:self.time_left.clone(),
+                    running: true,
+                    callback:self.callback.clone(),
+                    timer_interval_id: id,
+                    timer_sound: self.timer_sound.clone()
+                }.into()
+            },
+            TimerAction::Stop => {
+                self.stop();
+                Self {
+                    time_left:self.time_left.clone(),
+                    running: false,
+                    callback:self.callback.clone(),
+                    timer_interval_id: -1,
+                    timer_sound: self.timer_sound.clone()
+                }.into()
+            },
+            TimerAction::SetCallback(callback) => {
+                Self {
+                    time_left: self.time_left.clone(),
+                    running: self.running,
+                    callback,
+                    timer_interval_id: self.timer_interval_id,
+                    timer_sound: self.timer_sound.clone()
                 }.into()
             }
         }
